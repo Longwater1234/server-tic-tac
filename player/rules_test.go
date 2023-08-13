23c75607e8f261238076fbdd9170bda2cd211064
Davis T <davisinyo@gmail.com>
2023-08-13 20:47:35 +0800

    fix error

--- player/rules_test.go
+++ player/rules_test.go
@@ -33,7 +33,7 @@ func TestPlayer_HasWon(t *testing.T) {
 				Name:  tt.fields.Name,
 				Cells: tt.fields.Vals,
 			}
-			if got := p.HasWon(); got != tt.want {
+			if got, _ := p.HasWon(); got != tt.want {
 				t.Errorf("HasWon() = %v, want %v", got, tt.want)
 			}
 		})
